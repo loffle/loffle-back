0c1430946c08d6e9670e66f8b14d212b7a26eb9d
leeyongjoo <2yongjooo@gmail.com>
2021-09-01 17:16:33 +0900

    Feat: add post, postcomment model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from file.models import File
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, related_name="post_user", on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.SET_NULL, null=True)  # File
+    like = models.ManyToManyField(User)
+
+
+class PostComment(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="post_comment_user", on_delete=models.CASCADE)
+    like = models.ManyToManyField(User)
+
+
+# class Review(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
+#
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     file = models.ForeignKey(File, on_delete=models.SET_NULL)
+#     raffle = models.ForeignKey(Raffle, on_delete=models.SET_NULL)
+#     like = models.ManyToManyField(User)
+#
+#
+# class ReviewComment(models.Model):
+#     content = models.CharField(max_length=200)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
+#
+#     review = models.ForeignKey(Review, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     like = models.ManyToManyField(User)
+
