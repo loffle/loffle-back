dba711a48bee2448baf880839fca0b34fddb2e18
leeyongjoo <2yongjooo@gmail.com>
2021-09-06 11:59:35 +0900

    Style: Add CommonModelSerializer, LikeFieldSerializer
    CommonModelSerializer, LikeFieldSerializer 만들어서 상속하고 테스트 마침

--- community/serializers.py
+++ community/serializers.py
@@ -1,14 +1,36 @@
+from abc import ABC
+
+from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import HyperlinkedIdentityField, PrimaryKeyRelatedField
-from rest_framework.serializers import ModelSerializer
+
+from rest_framework.serializers import ModelSerializer, BaseSerializer, Serializer
 
 from community.models import Post, PostComment, Review, ReviewComment, Notice, Question, Answer
 
 from community.serializer_fields import CommentListUrlField, CommentDetailUrlField
 
 
+class CommonModelSerializer(ModelSerializer):
+    class Meta:
+        exclude = ('is_deleted',)
+        read_only_fields = ('user',)
+
+
+class LikeFieldSerializer(Serializer):
+    like_count = SerializerMethodField()
+    like_or_not = SerializerMethodField()
+
+    @staticmethod
+    def get_like_count(obj):
+        return obj.like.count()
+
+    def get_like_or_not(self, obj):
+        return self.context['request'].user in obj.like.all()
+
+
 # -------------------------------
 
-class PostCommentListSerializer(ModelSerializer):
+class PostCommentListSerializer(CommonModelSerializer):
     url = CommentDetailUrlField(view_name='post-comment-detail', read_only=True)
 
     class Meta:
@@ -17,23 +39,24 @@ class PostCommentListSerializer(ModelSerializer):
         read_only_fields = ('post', 'user', 'like')
 
 
-class PostCommentDetailSerializer(ModelSerializer):
+class PostCommentDetailSerializer(CommonModelSerializer):
     class Meta:
         model = PostComment
         exclude = ('is_deleted',)
         read_only_fields = ('post', 'user', 'like')
 
 
-class PostListSerializer(ModelSerializer):
+class PostListSerializer(CommonModelSerializer, LikeFieldSerializer):
     url = HyperlinkedIdentityField(view_name='post-detail', read_only=True)
 
-    class Meta:
+    class Meta(CommonModelSerializer.Meta):
         model = Post
-        exclude = ('is_deleted',)
-        read_only_fields = ('user', 'like')
+        # exclude = ('is_deleted',)
+        # read_only_fields = ('user', 'like')
+        read_only_fields = CommonModelSerializer.Meta.read_only_fields + ('like',)
 
 
-class PostDetailSerializer(ModelSerializer):
+class PostDetailSerializer(CommonModelSerializer, LikeFieldSerializer):
     comment_url = CommentListUrlField(view_name='post-comment-list', read_only=True)
     comments = PostCommentListSerializer(many=True, read_only=True)
 
@@ -43,35 +66,35 @@ class PostDetailSerializer(ModelSerializer):
         read_only_fields = ('user', 'like')
 
 
-class ReviewSerializer(ModelSerializer):
+class ReviewSerializer(CommonModelSerializer):
     class Meta:
         model = Review
         exclude = ('is_deleted',)
         read_only_fields = ('user', 'like')
 
 
-class ReviewCommentSerializer(ModelSerializer):
+class ReviewCommentSerializer(CommonModelSerializer):
     class Meta:
         model = ReviewComment
         exclude = ('is_deleted',)
         read_only_fields = ('review', 'user', 'like')
 
 
-class NoticeSerializer(ModelSerializer):
+class NoticeSerializer(CommonModelSerializer):
     class Meta:
         model = Notice
         exclude = ('is_deleted',)
         read_only_fields = ('user',)
 
 
-class QuestionSerializer(ModelSerializer):
+class QuestionSerializer(CommonModelSerializer):
     class Meta:
         model = Question
         exclude = ('is_deleted',)
         read_only_fields = ('user',)
 
 
-class AnswerSerializer(ModelSerializer):
+class AnswerSerializer(CommonModelSerializer):
     class Meta:
         model = Answer
         exclude = ('is_deleted',)
