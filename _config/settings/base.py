3cc8bfdf85041e5e2fed8db71cd8b60582b580b2
leeyongjoo <2yongjooo@gmail.com>
2021-09-07 23:03:58 +0900

    Feat: Add eb config

--- _config/settings/base.py
+++ _config/settings/base.py
@@ -16,14 +16,11 @@ from json import loads
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-with open(BASE_DIR / 'secrets.json', 'r') as f:
-    SECRETS = loads(f.read())
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRETS['SECRET_KEY']
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
