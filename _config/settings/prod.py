272d15f14bcb7d5a14281cdfabcfff6f8ba6155d
leeyongjoo <2yongjooo@gmail.com>
2021-09-11 18:48:36 +0900

    Fix: solve health check 4xx error

--- _config/settings/prod.py
+++ _config/settings/prod.py
@@ -27,5 +27,34 @@ if 'RDS_HOSTNAME' in os.environ:
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
+
         }
     }
+
+
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    import requests
+    if not is_ec2_linux():
+        return None
+    try:
+        response = requests.get('http://172.31.17.216/latest/meta-data/local-ipv4')
+        return response.text
+    except:
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
