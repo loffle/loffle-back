a8ea28a8d3c4880a25a0f81b8a7cb270cae5c1f5
leeyongjoo <2yongjooo@gmail.com>
2021-09-23 14:28:25 +0900

    Feat: Add RDS settings
    RDS 설정 추가

--- _config/settings/dev.py
+++ _config/settings/dev.py
@@ -10,3 +10,18 @@ SECRET_KEY = SECRETS['SECRET_KEY']
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
 ]
+
+if 'RDS_HOSTNAME' in SECRETS:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': SECRETS['RDS_DB_NAME'],
+            'USER': SECRETS['RDS_USERNAME'],
+            'PASSWORD': SECRETS['RDS_PASSWORD'],
+            'HOST': SECRETS['RDS_HOSTNAME'],
+            'PORT': SECRETS['RDS_PORT'],
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            }
+        }
+    }
