c0aa79cb3da606dcea7cd5ef90e15f894fc3d619
leeyongjoo <2yongjooo@gmail.com>
2021-09-30 18:05:04 +0900

    Feat: Add community models to the admin page
    admin 페이지에 community 모델들 추가

--- community/admin.py
+++ community/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from community.models import Post, PostComment, QuestionType
+from community.models import Post, PostComment, QuestionType, ReviewComment, Review, Question, Answer, Notice
 
 admin.site.register(Post)
 admin.site.register(PostComment)
+admin.site.register(Review)
+admin.site.register(ReviewComment)
 admin.site.register(QuestionType)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Notice)
