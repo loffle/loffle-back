1b0843c849c6383d627f11050d8e2f7997861a33
leeyongjoo <2yongjooo@gmail.com>
2021-10-10 17:39:31 +0900

    Feat: Add product CRUD

--- loffle/permissions.py
+++ loffle/permissions.py
@@ -10,3 +10,14 @@ class IsSuperuserOrReadOnly(BasePermission):
             return True
 
         return bool(request.user and request.user.is_superuser)
+
+
+class IsStaffOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(request.user and request.user.is_staff)
