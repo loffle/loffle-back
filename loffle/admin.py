09fb7fa945c195a33d861ab1823c30060da651c7
leeyongjoo <2yongjooo@gmail.com>
2021-10-18 12:43:30 +0900

    Feat: Add models of the roffle app to the admin page
    관리자 페이지에 로플 앱 모델들 추가

--- loffle/admin.py
+++ loffle/admin.py
@@ -1,6 +1,14 @@
+import sys, inspect
+
 from django.contrib import admin
 
-from loffle.models import Ticket, RaffleApply
+# from loffle.models import Ticket, RaffleApply
 
-admin.site.register(Ticket)
-admin.site.register(RaffleApply)
+# admin.site.register(Ticket)
+# admin.site.register(RaffleApply)
+
+import loffle.models
+
+for model_name, model_cls in inspect.getmembers(sys.modules[loffle.models.__name__], inspect.isclass):
+    if hasattr(model_cls, '_meta') and model_cls._meta.app_label == __package__:
+        admin.site.register(model_cls)
